06d8ca96007de735faf73e63e311742c45d40b2f
Ivan Lanin <ivanlanin@gmail.com>
2013-12-22 23:27:40 +0700

    Change wordwrap

--- google-audio-tag.php
+++ google-audio-tag.php
@@ -6,13 +6,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Audio</title>
 <style>
-audio { display: none; }
+    audio { display: none; }
 </style>
+<script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
 <body>
-<a id="hear"><input width="16" type="image" height="16" style="height:16px;width:16px" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAcUlEQVQ4y2P4//8/AyUYQhAH3gNxA7IAIQPmo/H3g/QA8XkgFiBkwHyoYnRQABVfj88AmGZcTuuHyjlgMwBZM7IE3NlQGhQe65EN+I8Dw8MLGgYoFpFqADK/YUAMwOsFigORatFIlYRElaRMWmaiBAMAp0n+3U0kqkAAAAAASUVORK5CYII=" /></a>
-<audio id="audio" controls="controls" src="//ssl.gstatic.com/dictionary/static/sounds/de/0/risk.mp3"></audio>
-<script src="./assets/js/jquery.min.js"></script>
+<a id="hear"><input width="16" type="image" height="16"
+    style="height:16px;width:16px"
+    src="data:image/png;base64,
+    iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAcUlEQVQ4y2P4//8/AyUYQhAH3gNx
+    A7IAIQPmo/H3g/QA8XkgFiBkwHyoYnRQABVfj88AmGZcTuuHyjlgMwBZM7IE3NlQGhQe65EN+I8D
+    w8MLGgYoFpFqADK/YUAMwOsFigORatFIlYRElaRMWmaiBAMAp0n+3U0kqkAAAAAASUVORK5CYII=
+    " /></a>
+<audio id="audio" controls="controls"
+    src="//ssl.gstatic.com/dictionary/static/sounds/de/0/risk.mp3"></audio>
 <script>
 $(function() {
 	$("#hear").click( function() {
