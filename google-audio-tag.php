25ed7dac03db07a3abccffd2180b960527fcf0b1
Ivan Lanin <ivanlanin@gmail.com>
2013-12-22 23:42:30 +0700

    Change image

--- google-audio-tag.php
+++ google-audio-tag.php
@@ -11,12 +11,24 @@
 <script src="//code.jquery.com/jquery-1.10.1.min.js"></script>
 </head>
 <body>
-<a id="hear"><input width="16" type="image" height="16"
-    style="height:16px;width:16px"
+<a id="hear"><input width="32" type="image" height="32"
+    style="height:32px;width:32px"
     src="data:image/png;base64,
-    iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAcUlEQVQ4y2P4//8/AyUYQhAH3gNx
-    A7IAIQPmo/H3g/QA8XkgFiBkwHyoYnRQABVfj88AmGZcTuuHyjlgMwBZM7IE3NlQGhQe65EN+I8D
-    w8MLGgYoFpFqADK/YUAMwOsFigORatFIlYRElaRMWmaiBAMAp0n+3U0kqkAAAAAASUVORK5CYII=
+    iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAAAAABWESUoAAAAAXNSR0IArs4c6QAAAARnQU1B
+    AACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAABBdEVYdENvbW1lbnQAQ1JFQVRPUjog
+    Z2QtanBlZyB2MS4wICh1c2luZyBJSkcgSlBFRyB2NjIpLCBxdWFsaXR5ID0gOTAKsEVYkwAA
+    ABp0RVh0U29mdHdhcmUAUGFpbnQuTkVUIHYzLjUuMTFH80I3AAACJ0lEQVQ4y4XTTUhUURjG
+    8f97x2bU1CFIIRywgQgq+nDRRpBo161FBYFk0KaCWki1k1xFRAg5YkGBiFCQKG1aVQRCltlq
+    JMxF9kGmSUQOGaioc8/7tpirjDrW2Z1zf3AOz31e7D+LtQduyGbdP8Ds+aPWcaAto4WBjtfh
+    WztUdiwUAm4gITmAHJ5060BwrwTwrXuTAMkPbjXQuXOeAL7pyAlPkORkPlA3XicA+GbqHpQJ
+    cmghBM59H3r1OCFILAK+OVXXXy5ISs0wc6/rIyIiSOxRHHzrbBxR1xOBrdNmmPYVA4AU9y6W
+    g2/tUnInCBoEWs3Qr/Hcd2J9LpsDELkfjEVhrzO0Gdh1M5VKvXAaAoGyT+4IRD4rWg9lk2Gy
+    OfD7mCBN7i7QY9h+2G2meUB/boFk9o3AdcX2wZ7lsELgTkJ05psHV6wQMHcGijJTHlzOXbEM
+    wkfq/HaoXBwWaFHsIFTNrnpk9pogDcFDgS5DL4A0DKbT6SkNQbcHkQF3FmRY0bdFIAJSPaor
+    QcnFIBOHxJJi2uyFUVe/XwqDktPzrkXgqhmm2dsVAgKSeFcBvqWSXVmXLoXYWO53B9O9t240
+    RQWpjoFvmUXViR2CXHJ5jQqeVbJSGB3dKVDza1Xl3MfaEOhcW1xg8+Da0s40eoBvz2sEKH0S
+    rKt9tjXKcu239QcFBsc9rQoLc2pizeCEWetYrW+pkuMv3UazqX867csP1Y2nW51p/v4vbhB3
+    +XxFgfkAAAAASUVORK5CYII=
     " /></a>
 <audio id="audio" controls="controls"
     src="//ssl.gstatic.com/dictionary/static/sounds/de/0/risk.mp3"></audio>
